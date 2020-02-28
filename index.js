afec7f2205c29a469db3702f090b449365286b82
Edu Moreno <edu.david2706@gmail.com>
2020-02-28 17:01:39 -0500

    first

---
+++ index.js
@@ -0,0 +1,42 @@
+'user strict'
+
+require('dotenv').config()
+
+const {
+    makeExecutableSchema
+} = require('graphql-tools')
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+const {
+    readFileSync
+} = require('fs')
+const {
+    join
+} = require('path')
+const cors = require("cors");
+const resolvers = require('./lib/resolver')
+
+const app = express()
+const port = process.env.port || 3000
+
+
+const typeDefs = readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8'
+)
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
+
+app.use(cors());
+app.use('/api', gqlMiddleware({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true
+}))
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}/api`)
+})
